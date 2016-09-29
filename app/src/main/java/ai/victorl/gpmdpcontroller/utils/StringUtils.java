ef13fa6824e910722026c866a4ebaaca839a9bf5
Victor Lai <lai.victor.vl@gmail.com>
2016-09-29 18:05:20 -0400

    Clean up files and ui

--- app/src/main/java/ai/victorl/gpmdpcontroller/utils/StringUtils.java
+++ app/src/main/java/ai/victorl/gpmdpcontroller/utils/StringUtils.java
@@ -3,9 +3,6 @@ package ai.victorl.gpmdpcontroller.utils;
 import android.bluetooth.BluetoothAdapter;
 
 public class StringUtils {
-    public static String DOT = ".";
-    public static String COLON = ".";
-
     public static String getDeviceName() {
         return BluetoothAdapter.getDefaultAdapter().getName();
     }
